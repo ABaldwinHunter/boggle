8365ddefb3eda3f15b7f402a3c7154c71e573a15
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2016-04-11 11:39:37 -0400

    remove blank tests

--- app/spec/boggle_board_spec.rb
+++ app/spec/boggle_board_spec.rb
@@ -3,26 +3,10 @@ require_relative "boggle_board"
 describe BoggleGame do
   describe "#initialize" do
     let(:valid_game) {BoggleGame.new}
-
     it "initializes with a board" do
       expect(valid_game.board).to_not be(nil)
     end
-
-    it "" do
-    end
-
-    it "" do
-    end
-
-    it "" do
-    end
-
-    it "" do
-    end
-
-    it "" do
-    end
-
+  end
 end
 
 # require_relative 'boggle_board'
